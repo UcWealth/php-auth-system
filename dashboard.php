218b51e09fa01e5dbb02a68fe79ec15b00202003
ucwealth <ucheokeke16@gmail.com>
2020-04-09 21:52:43 +0100

    edit dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,13 +7,12 @@ if(!isset($_SESSION['loggedIn'])){
     <h3>Dashboard</h3>
     <p>Welcome, <strong><?php echo $_SESSION['fullname']?></strong><br>You are logged in as a <strong><?php echo $_SESSION['role'] ?></strong> with User ID: <strong><?php echo $_SESSION['loggedIn'] ?></strong>
     </p>
-    <p><strong>Login time:</strong> <?php echo $_SESSION['login_time']?> </p>
 
     <div>
       <p><strong>User Access Level:</strong> <?php echo $_SESSION['role']?></p>
       <p><strong>Department:</strong> <?php echo $_SESSION['department']?></p>
       <p><strong>Date of registration: </strong><?php echo $_SESSION['date_registered'] ?></p>
-      <p><strong>Date of Last Login:</strong> </p>
+      <p><strong>Login time:</strong> <?php echo $_SESSION['login_time']?> </p>
     </div>
     
 <?php include('lib/footer.php'); ?>
